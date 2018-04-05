fddce73f27736e016029a24b12e08f7cd628853d
Edi Carlos note <edi_csilva@hotmail.com>
2018-04-05 12:14:47 -0300

    removendo os logs

--- public/app/app.js
+++ public/app/app.js
@@ -7,9 +7,7 @@ document
     fetch('http://localhost:3000/notas')
     .then(handleStatus)
     .then(notas => notas.$flatMap(notas => notas.itens))
-    .then(log)
     .then(itens => itens.filter(item => item.codigo == '2143'))
-    .then(log)
     .then(itens => itens.reduce((total, item) => total + item.valor, 0))
     .then(total => console.log(total))
     .catch(console.log);
