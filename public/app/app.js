3ff42d70174d2918c9c974f71f95c5ea0263246f
Edi Carlos note <edi_csilva@hotmail.com>
2018-04-05 12:05:26 -0300

    usado Higher-order Functions para simplicar o codigo

--- public/app/app.js
+++ public/app/app.js
@@ -1,16 +1,17 @@
-import { handleStatus } from './utils/promise-helpers.js';
+import { handleStatus, log } from './utils/promise-helpers.js';
 
 document
     .querySelector('#myButton')
     .onclick = () =>
     fetch('http://localhost:3000/notas')
     .then(handleStatus)
-    .then(notas => notas.map(nota => nota.itens))
-    .then(itens => {
-        console.log(itens);
-        return itens;
-    })
+    // achatar em uma dimensao os itens de cada nota
+    .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
+    .then(log)
+    // retornará para o próximo then uma lista de itens filtrada
     .then(itens => itens.filter(item => item.codigo == '2143'))
+    // retornará para o próximo then o total 
+    .then(log)
     .then(itens => itens.reduce((total, item) => total + item.valor, 0))
     .then(total => console.log(total))
     .catch(console.log);
