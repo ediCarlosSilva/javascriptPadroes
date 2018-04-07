da87f9d00497d494ae1e44e481b346e6a19e1dc3
Edi Carlos note <edi_csilva@hotmail.com>
2018-04-07 11:07:21 -0300

    criado a função pipe para para usar os parametros da composição da esquerda para a direita

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,5 +1,5 @@
 import { handleStatus } from '../utils/promise-helpers.js';
-import { partialize, compose } from '../utils/operators.js';
+import { partialize, pipe } from '../utils/operators.js';
 
 const API = `http://localhost:3000/notas`;
 
@@ -25,7 +25,11 @@ export const notasService = {
         // utilizando o partialize que foi importado
         const filterItems = partialize(filterItemsByCode, code);
         // realizando a composição
-        const sumItems = compose(sumItemsValue, filterItems, getItemsFromNotas);
+        const sumItems = pipe(
+            getItemsFromNotas,
+            filterItems,
+            sumItemsValue
+        );
 
         return this.listAll().then(sumItems);
     }
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -4,3 +4,7 @@ export const partialize = (fn, ...args) =>
 export const compose = (...fns) => value =>
     fns.reduceRight((previusValue, fn) =>
         fn(previusValue), value);
+
+export const pipe = (...fns) => value =>
+    fns.reduce((previusValue, fn) =>
+        fn(previusValue), value);
