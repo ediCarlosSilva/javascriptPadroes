fb5b8f58248b5aafa487b89555919b27f5a62be4
Edi Carlos note <edi_csilva@hotmail.com>
2018-04-10 17:14:43 -0300

    implementacao da funcao debounceTime versao1

--- public/app/app.js
+++ public/app/app.js
@@ -4,12 +4,6 @@ import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime } from './utils/operators.js';
 
-const showMessage = () => console.log('Opa!');
-const operation2 = debounceTime(500, showMessage);
-operation2();
-operation2();
-operation2();
-
 const operation1 = takeUntil(3, () =>
     service
     .sumItems('2143')
@@ -17,7 +11,8 @@ const operation1 = takeUntil(3, () =>
     .catch(console.log)
 );
 
+const operation2 = debounceTime(500, operation1);
 
 document
     .querySelector('#myButton')
-    .onclick = operation1;
+    .onclick = operation2;
