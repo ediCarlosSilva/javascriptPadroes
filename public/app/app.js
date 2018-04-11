f2d422428cc442b4ad578554b39fde8a061c77d8
Edi Carlos note <edi_csilva@hotmail.com>
2018-04-11 09:31:13 -0300

    feito a composição das funções takeuntil, partialize com pipe

--- public/app/app.js
+++ public/app/app.js
@@ -2,14 +2,19 @@ import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 // importa dando um apelido para o artefato importado
 import { notasService as service } from './nota/service.js';
-import { takeUntil, debounceTime } from './utils/operators.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
-const action = debounceTime(500, takeUntil(3, () =>
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+)
+
+const action = operations(() =>
     service
     .sumItems('2143')
     .then(console.log)
     .catch(console.log)
-));
+);
 
 document
     .querySelector('#myButton')
