8a31f920523a4015702167c013b5f81cffeb2db6
Edi Carlos note <edi_csilva@hotmail.com>
2018-04-11 09:20:57 -0300

    deixando codigo mais enxuto

--- public/app/app.js
+++ public/app/app.js
@@ -4,15 +4,13 @@ import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime } from './utils/operators.js';
 
-const operation1 = takeUntil(3, () =>
+const action = debounceTime(500, takeUntil(3, () =>
     service
     .sumItems('2143')
     .then(console.log)
     .catch(console.log)
-);
-
-const operation2 = debounceTime(500, operation1);
+));
 
 document
     .querySelector('#myButton')
-    .onclick = operation2;
+    .onclick = action;
