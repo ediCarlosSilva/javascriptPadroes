4d201f53fd7df52016e987ed871848fd4251cad4
Edi Carlos note <edi_csilva@hotmail.com>
2018-04-16 19:33:53 -0300

    criando um monada para encapsular o valor recebido como parametro

--- public/app/app.js
+++ public/app/app.js
@@ -4,6 +4,20 @@ import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
+import { EventEmitter } from './utils/event-emitter.js';
+
+import { Maybe } from './utils/maybe.js';
+
+const maybe1 = new Maybe(10);
+
+const maybe = Maybe
+    .of(10)
+    .map(value => value + 10)
+    .map(value => value + 30)
+    .getOrElse(0);
+
+alert(maybe);
+
 const operations = pipe(
     partialize(takeUntil, 3),
     partialize(debounceTime, 500)
@@ -11,7 +25,7 @@ const operations = pipe(
 
 const action = operations(() =>
     retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
-    .then(console.log)
+    .then(total => EventEmitter.emit('itensTotalizados', total))
     .catch(console.log)
 );
 
